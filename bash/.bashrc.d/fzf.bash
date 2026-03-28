9beb7eae10815376245ddf93105064573c72d7f2
Abhishek Rana <abhishekrana502@gmail.com>
2026-03-28 12:14:11 +0100

    Use fd as fzf file source for faster fuzzy finding

--- bash/.bashrc.d/fzf.bash
+++ bash/.bashrc.d/fzf.bash
@@ -1,1 +1,10 @@
-command -v fzf &>/dev/null && eval "$(fzf --bash)"
+command -v fzf &>/dev/null || return
+
+# Use fd for faster file listing (respects .gitignore)
+if command -v fd &>/dev/null; then
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+    export FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
+fi
+
+eval "$(fzf --bash)"
