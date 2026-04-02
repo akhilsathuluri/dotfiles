6a67968e405ad578ab0b95b2ecc6a39f369f7070
Abhishek Rana <abhishekrana502@gmail.com>
2026-04-02 10:03:59 +0200

    Improve search and visual highlight visibility for Solarized Light

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,17 @@ return {
     "maxmx03/solarized.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      on_highlights = function(colors, color)
+        ---@type table<string, vim.api.keyset.highlight>
+        return {
+          Visual = { bg = "#eee8d5", fg = "#002b36" },
+          Search = { bg = "#93A1A1", fg = "#fdf6e3" },
+          IncSearch = { bg = "#B58900", fg = "#fdf6e3" },
+          CurSearch = { bg = "#B58900", fg = "#fdf6e3" },
+        }
+      end,
+    },
   },
 
   -- Tell LazyVim to use it
